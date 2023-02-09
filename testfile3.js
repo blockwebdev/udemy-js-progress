26bd30750712855326debe8dfbdbac6902172650
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-02-08 21:57:30 -0500

    practice

---
+++ testfile3.js
@@ -0,0 +1,55 @@
+'use strict';
+// DATA
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+  
+const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+  
+const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+  
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+  
+const accounts = [account1, account2, account3, account4];
+
+const createUsernames = function (accs) {
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(" ")
+            .map(name => name[0])
+            .join("")
+    });
+}
+createUsernames(accounts);
+console.log(accounts);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// FILTER METHOD
+const deposits = movements.filter(function (mvmt) {  // regular function, and..
+    return mvmt > 0;
+});
+console.log(deposits);
+
+const withdrawals = movements.filter(mvmt => mvmt < 0);  // arrow function..(same job)
+console.log(withdrawals);
+
