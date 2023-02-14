5a8d389f0647c6336bee2147097af55c19d2e086
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-02-13 23:59:09 -0500

    Update testfile3.js

--- testfile3.js
+++ testfile3.js
@@ -1,3 +1,5 @@
+// MUTIPLE PROJECTS AND CHALLENGES
+
 'use strict';
 // DATA
 const account1 = {
@@ -52,4 +54,112 @@ console.log(deposits);
 
 const withdrawals = movements.filter(mvmt => mvmt < 0);  // arrow function..(same job)
 console.log(withdrawals);
+
+//REDUCE METOD
+const balance = movements.reduce(function (acc, val, i) {
+    console.log(`Iteration ${i}: ${val}`)
+    return acc + val;
+}, 0);
+console.log(balance);
+
+// Now, the same code as above but using the Arrow Function
+const balanceWithArrowFunc = movements.reduce((acc, val) => acc + val, 1);
+console.log(balanceWithArrowFunc);
+
+// MAXIMUM VALUE WITH ARROW FUNCTION
+const max = movements.reduce((acc, move) => acc > move ? acc : move);
+console.log(max);
+
+// MAXIMUM VALUE WITHOUT ARROW FUNCTION
+const max2 = movements.reduce(function (acc, mov) {
+    if (acc > mov)
+        return acc
+    else return mov;
+});
+console.log(max2 + 1);
+
+const calcAverageHumanAge = function (ages) {
+    const humanAges = ages.map(
+        age => age <= 2 ? 2 * age : 16 + age * 4
+    );
+    const adults = humanAges.filter(age => age >= 18);
+    const average = adults.reduce((acc, age) => acc
+        + age, 0) / adults.length;
+    return average;
+};
+const firstCalc = Math.floor(calcAverageHumanAge([15, 16, 18, 9, 1, 2, 3]));
+const secondCalc = calcAverageHumanAge([90, 4, 12, 7, 18, 56, 30]);
+console.log(firstCalc, secondCalc);
+
+const calcDisplaySummary = function (movements) {
+    const moneyIn = movements.filter(mov => mov > 0).reduce(    // Deposits
+        (acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `${moneyIn}`;
+
+    const moneyOut = movements.filter(mov => mov < 0).reduce(   // Withdrawals
+        (acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `${Math.abs(moneyOut)}`;
+
+    const interest = movements                                  // Interest
+        .filter(mov => mov > 0)
+        .map(deposit => deposit * 1.2 / 100)
+        .filter((int, i, arr) => {
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}`;
+};
+calcDisplaySummary(account1.movements);
+
+
+
+const calcAverageHumanAge = function (ages) {
+    const humanAges = ages.map(
+        age => age <= 2 ? 2 * age : 16 + age * 4
+    );
+    const adults = humanAges.filter(age => age >= 18);
+    const average = adults.reduce((acc, age) => acc
+        + age, 0) / adults.length;
+    return average;
+};
+
+const calcAverageHumanAge2 = ages => ages
+    .map(age => age <= 2 ? 2 * age : 16 + age * 4)
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+*/
+
+// EVENT HANDLER 
+let currentAccount; // -- must be outside of the function!!
+
+// attach the click event function to the login button 
+btnLogin.addEventListener("click", function (e) {
+    // Prevent form from immediately submitting
+    e.preventDefault;
+    // find is the best method for this use-case, to locate the username.
+    currentAccount = account.find(
+        acc => acc.username === inputLoginUsername.value
+    );
+
+    if (currentAccount?.pin === Number(inputLoginPin.value)) {
+        // Display UI and message
+        labelWelcome.textContent = `Welcome back 
+        ${currentAccount.owner.split(" ")[0]}`;
+
+        containerApp.style.opacity = 100;
+
+        // Clear input fields
+        inputLoginUsername.value = inputLoginPin.value = "";
+        inputLoginPin.blur();
+
+        // Display movements (transfers)
+        displayMovements(currentAccount.movements);
+
+        // Display balance
+        calcDisplayBalance(currentAccount.movements);
+
+        // Display summary
+        calcDisplaySummary(currentAccount);
+    }
+});
 
