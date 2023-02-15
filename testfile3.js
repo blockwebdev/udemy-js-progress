af8875ef205d8cdd47da23a15b9d472586d8fc96
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-02-14 23:51:52 -0500

    receipt tracker app

--- testfile3.js
+++ testfile3.js
@@ -1,4 +1,5 @@
-// MUTIPLE PROJECTS AND CHALLENGES
+/* code being reworked-through/improved weekly as courses/studies completed
+mixed examples go-alongs*/
 
 'use strict';
 // DATA
@@ -29,8 +30,53 @@ const account4 = {
     interestRate: 1,
     pin: 4444,
 };
-  
+
+// A SINGLE ARRAY OF ALL THE DATA
 const accounts = [account1, account2, account3, account4];
+
+// ALL OF THE ELEMENTS
+/*
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+// FUNCTIONS
+const displayMovements = function (movements, sort = false) {
+    containerMovements.innerHTML = '';
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+    movs.forEach(function (mov, i) {
+        const type = mov > 0 ? "deposit" : "withdrawal";
+        const html = `
+            <div class="movements__row">
+                <div class="movements__type movements__type--${type}">${i + 1
+            } ${type}</div>
+            <div class="movements__value">${mov}€</div>
+        </div>
+        `;
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
+};
 
 const createUsernames = function (accs) {
     accs.forEach(function (acc) {
@@ -42,7 +88,17 @@ const createUsernames = function (accs) {
     });
 }
 createUsernames(accounts);
-console.log(accounts);
+
+const updateUI = function(acc) {
+    // Display movements (transfers)
+    displayMovements(acc.movements);
+
+    // Display balance
+    calcDisplayBalance(acc);
+
+    // Display summary
+    calcDisplaySummary(acc);
+};
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -127,7 +183,6 @@ const calcAverageHumanAge2 = ages => ages
     .map(age => age <= 2 ? 2 * age : 16 + age * 4)
     .filter(age => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-*/
 
 // EVENT HANDLER 
 let currentAccount; // -- must be outside of the function!!
@@ -152,14 +207,30 @@ btnLogin.addEventListener("click", function (e) {
         inputLoginUsername.value = inputLoginPin.value = "";
         inputLoginPin.blur();
 
-        // Display movements (transfers)
-        displayMovements(currentAccount.movements);
-
-        // Display balance
-        calcDisplayBalance(currentAccount.movements);
-
-        // Display summary
-        calcDisplaySummary(currentAccount);
+        updateUI(currentAccount);
     }
 });
+*/
 
+// TRANSFER FUNDS - TO,AMOUNT,BUTTON -
+btnTransfer.addEventListener("click", function (e) {
+    e.preventDefault();
+    const amount = Number(inputTransferAmount.value);
+    const receiverAcc = accounts.find(
+        acc => acc.username === inputTransferTo.value
+    );
+    inputTransferAmount.value = inputTransferTo.value = ""; // clears the boxes
+
+    if (amount > 0 &&
+        receiverAcc && 
+        currentAccount.balance >= amount &&
+        receiverAcc?.username !== currentAccount.username
+    ) {
+        // DO THE TRANSFER
+        currentAccount.movements.push(-amount);
+        receiverAcc.movements.push(amount);
+
+        // UPDATE THE UI
+        updateUI(currentAccount);
+    }
+});
