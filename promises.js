84db1df3e321487e6edd5c898e584e0592555849
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-07-03 22:56:37 -0400

    Promises Labs

--- promises.js
+++ promises.js
@@ -1,5 +1,30 @@
 // Continuation from May/June Labs - in progress..
 
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement('.img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+        img.addEventListener('error', function () {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+let currentImg;
+
 // PART 1
 const loadNPause = async function () {
     try {
