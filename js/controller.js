130e9d8875d96c4f51252bea562fe67b75d6bab4
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-08-22 23:43:50 -0400

    bookmark local storage imp.

--- js/controller.js
+++ js/controller.js
@@ -23,15 +23,19 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
-    // 1) LOADING RECIPE
+
+    // 2) LOADING RECIPE
     await model.loadRecipe(id); // recipe is loaded here and will..
 
-    // 2) Rendering recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe); // data received from loadRecipe(id)
-    
+
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
   
 };
@@ -85,7 +89,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
