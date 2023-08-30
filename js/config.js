9d14d356afb14ee40d2b3f58671950f7e4801526
Chris A.R <108549749+blockwebdev@users.noreply.github.com>
2023-08-29 23:33:27 -0400

    api post data fin.

--- js/config.js
+++ js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '8f86b9cd-c414-473b-864d-4c36cf5d3a41';
+export const MODAL_CLOSE_SEC = 2.5;
--- js/helpers.js
+++ js/helpers.js
@@ -11,15 +11,34 @@ const timeout = function (s) {
 };
 
 export const getJSON = async function (url) {
-    try {
-        const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
-        const data = await res.json();
+  try {
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  };
+};
+
+export const sendJSON = async function (url, uploadData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(uploadData)
+    });
     
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-        return data;
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
 
-    } catch (err) {
-        throw err;
-    }
-
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  };
 };
